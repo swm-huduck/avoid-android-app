7a912f4e02d309ad7ccab580740541495a18ffc3
yucheon6000 <yucheon6000@gmail.com>
2021-07-21 20:59:07 +0900

    내비게이션 메인 화면에서 사용자 위치 표시 gradle 종속성 추가 (위치 관련)

--- app/src/main/java/com/huduck/application/fragment/navigation/NavigationMainFragment.java
+++ app/src/main/java/com/huduck/application/fragment/navigation/NavigationMainFragment.java
@@ -1,10 +1,13 @@
 package com.huduck.application.fragment.navigation;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
@@ -19,11 +22,25 @@ import com.huduck.application.R;
 import com.huduck.application.activity.MainActivity;
 import com.huduck.application.fragment.PageFragment;
 import com.huduck.application.service.NavigationService;
+import com.naver.maps.map.LocationSource;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.util.FusedLocationSource;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
 public class NavigationMainFragment extends PageFragment {
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,26 +80,22 @@ public class NavigationMainFragment extends PageFragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_navigation_main, container, false);
 
-        LinearLayout linearLayoutTmap = view.findViewById(R.id.LinearLayout_map);
+        // T MAP API KEY 등록
         TMapView tMapView = new TMapView(view.getContext());
-
         tMapView.setSKTMapApiKey( "l7xxf21cc9e0068d4fbbb7c939aa6bda5a25" );
-        linearLayoutTmap.addView( tMapView );
 
-        TMapMarkerItem mapMarker = new TMapMarkerItem();
-        mapMarker.setPosition(0.5f, 1f);
-        mapMarker.setVisible(TMapMarkerItem.VISIBLE);
-
-
-        tMapView.addMarkerItem("myMarker", mapMarker);
-        Fragment fragment = this;
-
-        NavigationService.addNavigationLocationChangeEvent((currentPoint, info) -> {
-            if(!fragment.isVisible()) return;
-            mapMarker.setTMapPoint(currentPoint);
-            tMapView.setCenterPoint(currentPoint.getLongitude(), currentPoint.getLatitude());
+        // 네이버 지도 현 위치 표시
+        MapView mapView = view.findViewWithTag("map_view");
+        mapView.getMapAsync(naverMap_ -> {
+            naverMap = naverMap_;
+            naverMap.getUiSettings().setLocationButtonEnabled(true);
+            locationSource = new FusedLocationSource(this, 1000);
+            naverMap.setLocationSource(locationSource);
+            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            naverMap.getLocationOverlay().setVisible(true);
         });
 
+        // 검색창 클릭 이벤트 등록
         TextView searchInput = view.findViewById(R.id.search_input);
         searchInput.setOnClickListener(new View.OnClickListener() {
             @Override
