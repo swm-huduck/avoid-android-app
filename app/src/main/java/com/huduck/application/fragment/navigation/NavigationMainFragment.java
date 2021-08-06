7c7a06a49935c3afcabbfe9945ac924ddc9bf79a
yucheon6000 <yucheon6000@gmail.com>
2021-08-06 15:22:43 +0900

    내비게이션 메인 페이지 미세 수정  나침반 UI 위치 수정  검색창 디자인 구현 방법 변경

--- app/src/main/java/com/huduck/application/fragment/navigation/NavigationMainFragment.java
+++ app/src/main/java/com/huduck/application/fragment/navigation/NavigationMainFragment.java
@@ -1,51 +1,27 @@
 package com.huduck.application.fragment.navigation;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
+import com.huduck.application.common.CommonMethod;
 import com.huduck.application.R;
 import com.huduck.application.activity.MainActivity;
 import com.huduck.application.fragment.PageFragment;
-import com.huduck.application.service.NavigationService;
-import com.naver.maps.map.LocationSource;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.util.FusedLocationSource;
-import com.skt.Tmap.TMapMarkerItem;
-import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
 public class NavigationMainFragment extends PageFragment {
     private FusedLocationSource locationSource;
     private NaverMap naverMap;
-
-    private static final int PERMISSION_REQUEST_CODE = 100;
-    private static final String[] PERMISSIONS = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-    };
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     public NavigationMainFragment() {}
 
@@ -57,10 +33,6 @@ public class NavigationMainFragment extends PageFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -79,14 +51,16 @@ public class NavigationMainFragment extends PageFragment {
         mapView.getMapAsync(naverMap_ -> {
             naverMap = naverMap_;
             naverMap.getUiSettings().setLocationButtonEnabled(true);
+            int[] paddings = naverMap.getContentPadding();
+            naverMap.setContentPadding(paddings[0], CommonMethod.dpToPx(getResources(), 56) ,paddings[2], paddings[3]);
+
             locationSource = new FusedLocationSource(this, 1000);
             naverMap.setLocationSource(locationSource);
             naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-            naverMap.getLocationOverlay().setVisible(true);
         });
 
         // 검색창 클릭 이벤트 등록
-        TextView searchInput = view.findViewById(R.id.search_input);
+        View searchInput = view.findViewById(R.id.search_input);
         searchInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
